d0f6cd92ca31cd9a2c4a8d61a1a61854c71c2f20
ClareKang <shiningkey89@gmail.com>
2017-05-31 17:02:31 +0900

    Update 2017-05-30 / 2017-05-31 winner

--- src/seed.js
+++ src/seed.js
@@ -4,68 +4,68 @@ export const SEED_BRACKET = [
       {
         round: 1,
         time: '15:00',
-        team1: 1,
-        team2: 2,
+        team1: 1,     // LM 운영팀 2
+        team2: 2,     // 인터페이스개발팀
         score1: 0,
         score2: 0,
-        winner: 2,
+        winner: 2,    // 인터페이스개발팀
       },
       {
         round: 1,
         time: '15:30',
-        team1: 3,
-        team2: 4,
+        team1: 3,     // 신사업TF
+        team2: 4,     // 연합팀(플랫폼개발/제휴영업/CS운영)
         score1: 234,
         score2: 427,
-        winner: 4,
+        winner: 4,    // 연합팀(플랫폼개발/제휴영업/CS운영)
       },
     ],
     "2017-5-30": [
       {
         round: 1,
         time: '15:00',
-        team1: 5,
-        team2: 6,
-        score1: 0,
-        score2: 0,
-        winner: null,
+        team1: 5,     // 전략기획실
+        team2: 6,     // 개발전략팀
+        score1: 360,
+        score2: 322,
+        winner: 5,    // 전략기획실
       },
       {
         round: 1,
         time: '15:30',
-        team1: 7,
-        team2: 8,
-        score1: 0,
-        score2: 0,
-        winner: null,
+        team1: 7,     // LM구축팀
+        team2: 8,     // 인프라개발팀
+        score1: 393,
+        score2: 265,
+        winner: 7,    // LM구축팀
       },
     ],
     "2017-5-31": [
       {
         round: 1,
         time: '15:00',
-        team1: 11,
-        team2: 12,
-        score1: 0,
-        score2: 0,
-        winner: null,
+        team1: 11,    // 운영관리팀
+        team2: 12,    // 솔루션사업팀
+        score1: 219,
+        score2: 270,
+        winner: 12,   // 솔루션사업팀
       },
       {
         round: 1,
         time: '15:30',
-        team1: 13,
-        team2: 14,
-        score1: 0,
-        score2: 0,
-        winner: null,
+        team1: 13,    // 영업본부
+        team2: 14,    // 솔루션기획팀
+        score1: 353,
+        score2: 316,
+        winner: 13,   // 솔루션기획팀
       },
     ],
     "2017-6-1": [
       {
         round: 1,
         time: '15:00',
-        team1: 15,
-        team2: 16,
+        team1: 15,    // 물류기획팀
+        team2: 16,    // 솔루션개발팀
         score1: 0,
         score2: 0,
         winner: null,
@@ -73,8 +73,8 @@ export const SEED_BRACKET = [
       {
         round: 1,
         time: '15:30',
-        team1: 17,
-        team2: 18,
+        team1: 17,    // 서비스기획팀
+        team2: 18,    // 로컬영업팀
         score1: 0,
         score2: 0,
         winner: null,
@@ -86,8 +86,8 @@ export const SEED_BRACKET = [
       {
         round: 2,
         time: '15:00',
-        team1: '2017-5-29|15:00',
-        team2: '2017-5-29|15:30',
+        team1: '2017-5-29|15:00', // 인터페이스개발팀
+        team2: '2017-5-29|15:30', // 연합팀(플랫폼개발/제휴영업/CS운영)
         score1: 0,
         score2: 0,
         winner: null,
@@ -95,8 +95,8 @@ export const SEED_BRACKET = [
       {
         round: 2,
         time: '15:30',
-        team1: '2017-5-30|15:00',
-        team2: '2017-5-30|15:30',
+        team1: '2017-5-30|15:00', // 전략기획실
+        team2: '2017-5-30|15:30', // LM구축팀
         score1: 0,
         score2: 0,
         winner: null,
@@ -106,8 +106,8 @@ export const SEED_BRACKET = [
       {
         round: 2,
         time: '15:00',
-        team1: '2017-5-31|15:00',
-        team2: '2017-5-31|15:30',
+        team1: '2017-5-31|15:00', // 솔루션사업팀
+        team2: '2017-5-31|15:30', // 영업본부
         score1: 0,
         score2: 0,
         winner: null,
@@ -128,8 +128,8 @@ export const SEED_BRACKET = [
       {
         round: 3,
         time: '15:00',
-        team1: 0,
-        team2: '2017-6-2|15:00',
+        team1: 0,                 // 디자인팀
+        team2: '2017-6-2|15:00',  // 
         score1: 0,
         score2: 0,
         winner: null,
@@ -137,8 +137,8 @@ export const SEED_BRACKET = [
       {
         round: 3,
         time: '15:30',
-        team1: '2017-6-2|15:30',
-        team2: 9,
+        team1: '2017-6-2|15:30',  // 
+        team2: 9,                 // LM 운영팀 1
         score1: 0,
         score2: 0,
         winner: null,
@@ -148,8 +148,8 @@ export const SEED_BRACKET = [
       {
         round: 3,
         time: '15:00',
-        team1: 10,
-        team2: '2017-6-5|15:00',
+        team1: 10,                // 경영지원실
+        team2: '2017-6-5|15:00',  // 
         score1: 0,
         score2: 0,
         winner: null,
@@ -157,8 +157,8 @@ export const SEED_BRACKET = [
       {
         round: 3,
         time: '15:30',
-        team1: '2017-6-5|15:30',
-        team2: 19,
+        team1: '2017-6-5|15:30',  // 
+        team2: 19,                // QA팀
         score1: 0,
         score2: 0,
         winner: null,
